5c29b429bcbdd5aa901ccc4a1ea7dff0df2e63a6
ldmfiel <ldmfiel@gmail.com>
2017-01-02 11:17:01 +0000

    All examples from lesson 1 and 2 complete

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,54 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-$("#main").append(["Leon Field"]);
+var role = "Level 23 Sorcerer";
+var name = "Leon Field";
+var contactInfo = "ldmfiel@outlook.com";
+var pictureUrl = "www.imageurl.com";
+var welcomemessage = "hello welcome to the site";
+var skills = ["awesomeness", "programming", "teaching", "JS"];
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+
+var bio = {
+    "name":name,
+    "role":role,
+    "contactInfo":contactInfo,
+    "pictureurl":pictureUrl,
+    "welcomemessage":welcomemessage,
+    "skills":skills  
+};
+
+var work = {
+    "job position" : "Lecturer",
+    "employer" : "SGS College",
+    "yearsworked" : 1,
+    "city" : "stroud uk"
+};
+
+var education = {
+	"lastschool": "Staffordshire University",
+	"yearstring": "2011-2014",
+
+	"schools": [{
+			"name": "Staffordshire University",
+			"city": "stafford, uk"
+		},
+
+		{
+			"name": "SGS College",
+			"city": "Stroud"
+		}
+	]
+}
+
+$("#header").prepend([formattedName]);
+$("#header").append([formattedRole]);
+
+$("#main").append(bio.name);
+$("#main").append(work["job position"]);
+$("#main").append(education.lastschool);
+
