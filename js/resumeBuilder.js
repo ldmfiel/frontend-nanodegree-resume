789f03ba0b9f500f3f42be48a6498dd3b0453de2
ldmfiel <ldmfiel@gmail.com>
2017-01-02 14:51:49 +0000

    End of Lesson 3 problems commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,32 +2,56 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-var role = "Level 23 Sorcerer";
-var name = "Leon Field";
-var contactInfo = "ldmfiel@outlook.com";
-var pictureUrl = "www.imageurl.com";
-var welcomemessage = "hello welcome to the site";
-var skills = ["awesomeness", "programming", "teaching", "JS"];
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
+/*-- Define JScript Object for resume */
 
 var bio = {
-    "name":name,
-    "role":role,
-    "contactInfo":contactInfo,
-    "pictureurl":pictureUrl,
-    "welcomemessage":welcomemessage,
-    "skills":skills  
+    "name": "Leon Field",
+    "role": "Lecturer",
+    "contacts": {
+                    "email" : "ldmfiel@outlook.com",
+                    "github" : "ldmfiel",
+                    "mobile" : "0000000000",
+                    "loation" : "Stroud, UK"
+                },
+    "pictureurl": "images/profilepic.jpg",
+    "welcomemessage": "Welcome to the thunder dome",
+    "skills":"Bending time and space"  
 };
 
 var work = {
-    "job position" : "Lecturer",
-    "employer" : "SGS College",
-    "yearsworked" : 1,
-    "city" : "stroud uk"
+        "jobs" : [
+            {
+                "employer" : "SGS College",
+                "title" : "Game Engines Lecturer",
+                "location" : "Stroud, uk",
+                "dates" : "September 2015 - Current",
+                "description" : "Working with students 16+ training them to develop games using Unreal Engine 4 as-well as teaching games design theory."
+            },
+            {
+                "employer" : "Bouncy Box",
+                "title" : "Indie Developer",
+                "location" : "Stafford, uk",
+                "dates" : "April 2013 - September 2014",
+                "description" : "Developed small web and mobile games in a small team of undergrads."
+            }
+    ]
+};
+
+var projects = {
+    "projects" : [
+        {
+            "title" : "Trousers",
+            "dates" : "September 2013",
+            "description" : "In Trousers you play as the simplistic yellow dude, P. Collecting a variety of clothing you avoid traps and jump on platforms making use of a variety of different abilities.",
+            "images": ["images/Level1.png", "images/new 2.png", "images/mountains.png"]            
+        },
+        {
+            "title" : "Wrapping",
+            "dates" : "January 2014-September 2014",
+            "description" : "Wrapping was a puzzle platformer using classic screen wrapping mechanics in a platforming title. A working prototype was created before the team moved onto other ventures.",
+            "images" : ["images/Boss.gif", "images/Introscene.gif", "images/Level1.gif"]          
+        }
+    ]
 };
 
 var education = {
@@ -43,8 +67,24 @@ var education = {
 			"name": "SGS College",
 			"city": "Stroud"
 		}
-	]
+	],
+    
+    "onlineCourses" : [
+        {
+            "title" : "Introduction to HTML and CSS",
+            "school" : "Udacity",
+            "url" : "https://www.udacity.com/course/intro-to-html-and-css--ud304"
+        
+        },
+        {
+            "title" : "Javascript Basics",
+            "school" : "Udacity",
+            "url" : "https://www.udacity.com/course/javascript-basics--ud804"
+        }
+    ]
 }
+
+/* jquery begins--> */
 
 $("#header").prepend([formattedName]);
 $("#header").append([formattedRole]);
